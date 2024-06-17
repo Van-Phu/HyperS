bef600f8b5446d9ebad53ddf293290e2265a9f82
Van Phu <luongphu08@gmail.com>
2024-06-17 17:35:50 +0700

    Add Libary Kendo UI

---
+++ src/app/ecom-pages/shared/dto/DTOImageProduct.ts
@@ -0,0 +1,6 @@
+export class DTOImageProduct {
+    Code: number = -1;
+    ImageUrl: string = 'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=';
+    IsThumbnail: boolean = false
+
+}
