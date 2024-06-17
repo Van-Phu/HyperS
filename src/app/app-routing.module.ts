a8d9c2e4e2319ac43e9a1794f3fe3cec6454499e
Van Phu <luongphu08@gmail.com>
2024-06-17 19:25:24 +0700

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'ecom', loadChildren: () => import('./ecom-pages/ecom-routing.module').then(m => m.EcomRoutingModule) },
+  { path: 'home', loadChildren: () => import('./ecom-pages/ecom-routing.module').then(m => m.EcomRoutingModule) },
   { path: 'admin', loadChildren: () => import('./admin-pages/admin-routing.module').then(m => m.AdminRoutingModule) },
+  // Catch-all route
+  { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
