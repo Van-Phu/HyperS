73bb0d6a547bcdc4d1fbb8006d9dcb3f10521113
doquocthanh <doquocthanh@github.com>
2024-06-19 13:57:43 +0700

    admin-pages shared add: Khởi tạo component button

---
+++ src/app/admin-pages/pages/admin001-information-customer/admin001-information-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin001-information-customer',
+  templateUrl: './admin001-information-customer.component.html',
+  styleUrls: ['./admin001-information-customer.component.scss']
+})
+export class Admin001InformationCustomerComponent {
+
+}
--- src/app/admin-pages/shared/components/button/button.component.ts
+++ src/app/admin-pages/shared/components/button/button.component.ts
@@ -1,13 +1,28 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'component-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
+
 export class ButtonComponent {
-  @Input() text: string = '';
-  @Input() width: number = 0;
-  @Input() height: number = 0;
-  @Input() bgColor: string = '';
+  @Input() text: string = 'Thêm mới';
+  @Input() classIconFontAwesome: string = 'fa-plus';
+  @Input() size: number = 14;
+  @Input() width: number;
+  @Input() height: number;
+  @Input() color: string = '#fff';
+  @Input() bgColor: string = '#0d6efd';
+  @Input() rounded: number = 3;
+  @Input() borderWidth: number;
+  @Input() borderColor: string;
+  @Input() borderStyle: string;
+  @Input() paddingVertical: number = 8;
+  @Input() paddingHorizontal: number = 12;
+  @Input() padding: number;
+  @Input() gap: number = 10;
+  @Input() boxShadow: string;
+  @Input() textStyle: string;
+  @Input() fontWeight: number = 600;
 }
