9b0ab9415e597c8558edb3057acee3beb3b552c2
Van Phu <luongphu08@gmail.com>
2024-06-21 08:28:40 +0700

    Revert "Merge pull request #23 from Van-Phu/minh"
    This reverts commit d2f36b3b3afcb9786b69bca2e593c064f8bdc764, reversing
    changes made to aa6fee30dc13dccdc7edea90d6ce66a821e7aefd.

--- src/app/admin-pages/pages/admin006-manage-cart/admin006-manage-cart.component.ts
+++ src/app/admin-pages/pages/admin006-manage-cart/admin006-manage-cart.component.ts
@@ -6,20 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin006-manage-cart.component.scss']
 })
 export class Admin006ManageCartComponent {
-  currentDate: Date = new Date();
-  minDate: Date = new Date(1900, 1, 1);
-  maxDate: Date = new Date(this.currentDate.getFullYear() + 50, 12, 30);
-  startDate: Date = this.minDate;
-  endDate: Date = this.maxDate;
-  // listStatus: [] = ['a'];
-  listStatus = ['Chờ xác nhận','Đang đóng gói','Đang vận chuyển','Thành công','Thất bại'];
 
-  getDateFromDatePicker(value: any, type: string){
-    if(type === 'start'){
-      this.startDate = value;
-    }
-    if(type === 'end'){
-      this.endDate = value;
-    }
-  }
 }
