b2a87c2b68be24c3eddcf08ee949b3ac36250599
doquocthanh <doquocthanh@github.com>
2024-06-21 10:12:58 +0700

    app - shared -  update: Thêm style boxshadow lại cho các component

--- src/app/shared/component/search-bar/search-bar.component.ts
+++ src/app/shared/component/search-bar/search-bar.component.ts
@@ -21,7 +21,7 @@ export class SearchBarComponent {
   @Input() rounded: number = 100;
   @Input() paddingLeft: number = 30;
   @Input() paddingRight: number = 10;
-  @Input() boxShadow: string = 'rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px';
+  @Input() boxShadow: string = 'rgba(0, 0, 0, 0.15) 0px 5px 15px';
   @Input() borderWidth: number;
   @Input() borderColor: string;
   @Output() getValue = new EventEmitter();
