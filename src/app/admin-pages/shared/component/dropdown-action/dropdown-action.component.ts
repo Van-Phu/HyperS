963aa7bbedae9997e467ca1b76bc3731877c010a
LeHuuMinh0509 <lehuuminhtc31@gmail.com>
2024-06-22 09:50:27 +0700

    app - admin-pages - pages - shared - dropdown-action - update: hoan thanh dropdown-action

--- src/app/admin-pages/shared/component/dropdown-action/dropdown-action.component.ts
+++ src/app/admin-pages/shared/component/dropdown-action/dropdown-action.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DTOStatus } from '../../dto/DTOStatus.dto';
 
 @Component({
@@ -6,11 +6,23 @@ import { DTOStatus } from '../../dto/DTOStatus.dto';
   templateUrl: './dropdown-action.component.html',
   styleUrls: ['./dropdown-action.component.scss']
 })
-export class DropdownActionComponent {
+export class DropdownActionComponent{
+
   @Input() textStyle: string;
   @Input() fontWeight: number = 600;
+  @Input() rounded: number =4;
   @Input() size: number = 14;
   @Input() text: string = 'Chờ xác nhận';
   @Input() classIconFontAwesome: string = 'fa-plus';
   @Input() listItem:DTOStatus[] = [];
+  @Output() sendValue = new EventEmitter();
+  
+
+  //truyen Output khi click item trong dropdown-action
+  onClickItemDropdown(value: any){
+    if(value){
+      alert(value)
+      this.sendValue.emit(value);
+    }
+  }
 }
