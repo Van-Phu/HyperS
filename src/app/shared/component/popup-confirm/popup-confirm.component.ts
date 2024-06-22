a7ac58b9ff268c1b81331dc2d74f24b689becb29
LeHuuMinh0509 <lehuuminhtc31@gmail.com>
2024-06-22 15:35:08 +0700

    app - shared - button - update: update thêm input jusity cho component button và hoàn thành component popup-confirm

--- src/app/shared/component/popup-confirm/popup-confirm.component.ts
+++ src/app/shared/component/popup-confirm/popup-confirm.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, ElementRef, Input } from '@angular/core';
 
+/**
+ * 
+ */
 @Component({
   selector: 'component-popup-confirm',
   templateUrl: './popup-confirm.component.html',
   styleUrls: ['./popup-confirm.component.scss']
 })
-export class PopupConfirmComponent {
+export class PopupConfirmComponent{
+  @Input() widthPopUp: number = 370;
+  @Input() heightPopUp: number = 238;
+  @Input() color: string = "#09880E";
+  @Input() fontWeightHeader: number = 600;
+  @Input() fontSizeHeader: number = 18;
+  // @Input() heightIconHeader: number = 26;
+  @Input() textHeader: string = 'Đóng gói';
+  @Input() sizeIcon: number = 26;
+  @Input() classIconFontAwesome: string = 'fa-triangle-exclamation';
+  @Input() heightHeader: number = 50;
+  @Input() gap: number = 10;
+
+
 
 }
