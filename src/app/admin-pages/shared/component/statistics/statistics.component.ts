3884eb5e24495f299ec3ad5566d2517f87c0aa81
doquocthanh <doquocthanh@github.com>
2024-06-27 00:39:22 +0700

    admin-pages - components  - fix: Chỉnh sửa cấu trúc statistics

--- src/app/admin-pages/shared/component/statistics/statistics.component.ts
+++ src/app/admin-pages/shared/component/statistics/statistics.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { trigger, style, animate, transition } from '@angular/animations';
 
+/**
+ * Component thống kê. Trong đó:
+ * - Input textField là tiêu đề.
+ * - Input valueField là số liệu cần hiển thị.
+ * - Input color là màu chủ đạo (là màu mã HEX).
+ */
 @Component({
   selector: 'component-statistics',
   templateUrl: './statistics.component.html',
@@ -17,9 +23,11 @@ import { trigger, style, animate, transition } from '@angular/animations';
 export class StatisticsComponent implements OnInit {
   @Input() textField: string = 'Nhập tiêu đề...';
   @Input() valueField: number = 100;
-  
+  @Input() color: string = '#2557A0';
+  boxShadow: string = `${this.hexToRgba(this.color, 0.3)} 0px 5px 10px`;
+
   currentCount: number = 0;
-  
+
   ngOnInit(): void {
     this.countUp();
   }
@@ -27,7 +35,7 @@ export class StatisticsComponent implements OnInit {
   countUp() {
     let start = 0;
     const end = this.valueField;
-    const duration = 500;
+    const duration = 1000;
     const increment = end / (duration / 10);
 
     const interval = setInterval(() => {
@@ -39,5 +47,24 @@ export class StatisticsComponent implements OnInit {
         this.currentCount = Math.round(start);
       }
     }, 10);
+  }
+
+  hexToRgba(hex: string, alpha: number = 1): string {
+    let r = 0, g = 0, b = 0;
+
+    // Handle 3-digit hex
+    if (hex.length === 4) {
+      r = parseInt(hex[1] + hex[1], 16);
+      g = parseInt(hex[2] + hex[2], 16);
+      b = parseInt(hex[3] + hex[3], 16);
+
+    // Handle 6-digit hex
+    } else if (hex.length === 7) {
+      r = parseInt(hex[1] + hex[2], 16);
+      g = parseInt(hex[3] + hex[4], 16);
+      b = parseInt(hex[5] + hex[6], 16);
+    }
+
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
   }
 }
