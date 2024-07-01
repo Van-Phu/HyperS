311138607b80725250ee9fa95e0b387d8450b9c9
thanhdev <doquocthanh@github.com>
2024-07-01 20:02:36 +0700

    admin-pages - pages/admin009-detail-product - add: Thêm list size

--- src/app/shared/component/text-dropdown/text-dropdown.component.ts
+++ src/app/shared/component/text-dropdown/text-dropdown.component.ts
@@ -15,7 +15,7 @@ export class TextDropdownComponent implements OnInit {
   @Input() rounded: number = 6;
   @Input() paddingLeft: number;
   @Input() paddingRight: number;
-  @Input() boxShadow: string = 'rgba(0, 0, 0, 0.15) 0px 5px 15px';
+  @Input() boxShadow: string = 'rgba(0, 0, 0, 0.1) 0px 0px 10px';
   @Input() borderWidth: number = 1;
   @Input() borderColor: string = '#EFEFEF';
   @Input() defaultItem: any;
--- src/app/shared/component/text-input/text-input.component.ts
+++ src/app/shared/component/text-input/text-input.component.ts
@@ -27,7 +27,7 @@ export class TextInputComponent implements OnInit {
   @Input() rounded: number = 5;
   @Input() paddingLeft: number;
   @Input() paddingRight: number = 0;
-  @Input() boxShadow: string = 'rgba(0, 0, 0, 0.15) 0px 5px 15px';
+  @Input() boxShadow: string = 'rgba(0, 0, 0, 0.1) 0px 0px 10px';
   @Input() borderWidth: number = 1;
   @Input() borderColor: string = '#B6B6B6';
   @Input() typeValue: 'text' | 'number' = 'text';
